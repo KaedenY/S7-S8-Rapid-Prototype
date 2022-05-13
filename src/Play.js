93571544b042cb53596ecda1092fc7fa62172f9b
Casey <caxchen@ucsc.com>
2022-05-13 09:52:53 -0700

    Co-authored-by: KaedenY <KaedenY@users.noreply.github.com>

--- src/Play.js
+++ src/Play.js
@@ -3,6 +3,12 @@ class Play extends Phaser.Scene {
         super("Play");
     }
 
+    preload() {
+        this.preload.image("placeholder_black", 'assets/placeholder_black.png');
+        this.preload.image('placeholder_grey', 'assets/placeholder_grey');
+        this.preload.image('placeholder_blue', 'assets/placeholder_blue.png');
+    }
+
     create() {}
 
     update() {}
