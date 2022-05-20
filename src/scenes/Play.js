6cdb24368647497e5e321b3eca3e506cfb4fac0d
bluemew0 <60424761+bluemew0@users.noreply.github.com>
2022-05-20 09:31:22 -0700

    title screen visuals, tweening, fade transition

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,17 +4,32 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.preload.image("placeholder_black", 'assets/placeholder_black.png');
-        this.preload.image('placeholder_grey', 'assets/placeholder_grey');
-        this.preload.image('placeholder_blue', 'assets/placeholder_blue.png');
+        this.load.image("bg", "./assets/bg.jpg");
+        this.load.image("floor", "./assets/bg_floor.png");
+        this.load.image("logs", "./assets/bg_logs.png");
+        this.load.image("sparkles", "./assets/bg_sparkles.png");
     }
 
     create() {
-        this.Roald = new this.player(this, game.config.width/2, game.config.height/2, 'placeholder_blue');
+        // this.Roald = new this.player(this, game.config.width/2, game.config.height/2, 'placeholder_blue');
+         // adding bg images
+         this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg").setOrigin(0);
+         this.add.tileSprite(0, 0, game.config.width, game.config.height, "sparkles").setOrigin(0);
+         this.logs = this.add.tileSprite(0, 0, game.config.width, game.config.height, "logs").setOrigin(0);
+         this.floor = this.add.tileSprite(0, 0, game.config.width, game.config.height, "floor").setOrigin(0);
+ 
+         this.SCROLL_SPEED = 2; // for parallax
+
+         // camera fade
+         this.cameras.main.fadeIn(350,100,100,100);
+        
     }
 
 
     update() {
-        return;
+        // parallax
+        this.bg.tilePositionX += this.SCROLL_SPEED;
+        this.logs.tilePositionX += this.SCROLL_SPEED+1;
+        this.floor.tilePositionX += this.SCROLL_SPEED+1;
     }
 }
--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -3,13 +3,95 @@ class Title extends Phaser.Scene {
         super("Title");
     }
 
+    preload(){
+        this.load.image("bg", "./assets/bg.jpg");
+        this.load.image("floor", "./assets/bg_floor.png");
+        this.load.image("logs", "./assets/bg_logs.png");
+        this.load.image("sparkles", "./assets/bg_sparkles.png");
+        this.load.image("title", "./assets/title.png");
+        this.load.image("Lfairy", "./assets/bg_leftFairy.png");
+        this.load.image("Rfairy", "./assets/bg_rightFairy.png");
+        this.load.image("rolypoly", "./assets/rolyPoly.png");
+    }
+
     create() {
+        // adding bg images
+        this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, "bg").setOrigin(0);
+        this.logs = this.add.tileSprite(0, 0, game.config.width, game.config.height, "logs").setOrigin(0);
+        this.add.tileSprite(0, 0, game.config.width, game.config.height, "sparkles").setOrigin(0);
+        this.floor = this.add.tileSprite(0, 0, game.config.width, game.config.height, "floor").setOrigin(0);
+        this.Lfairy = this.add.image(0, 0, "Lfairy").setOrigin(0);
+        this.Rfairy = this.add.image(0, 0, "Rfairy").setOrigin(0);
+        this.title = this.add.image(50, 0, "title").setOrigin(0);
+        this.rolypoly = this.add.sprite(game.config.width/2, 526, "rolypoly").setScale(0.3).setInteractive();
+
+        // when rolypoly is clicked
+        this.rolypoly.on("pointerup", () => {
+            // fairy tween
+            this.tweens.add({
+                targets: this.Lfairy,
+                x: -500,
+                duration: 600
+            })
+            this.tweens.add({
+                targets: this.Rfairy,
+                x: 500,
+                duration: 600
+            })
+
+            // camera fade
+            this.time.delayedCall(300, () => {this.cameras.main.fade(300,100,0,100);});
+
+            // title tween
+            this.tweens.add({
+                targets: this.title,
+                y: -500,
+                duration: 600,
+                onComplete: () => {
+                    this.scene.start('Play');
+                }
+            })
+        })
+
+        this.SCROLL_SPEED = 2; // for parallax
+         
+        // tweening
+        this.tweens.add({
+            targets: this.title,
+            y: "-= 10",
+            repeat: -1,
+            yoyo: true
+        });
+        this.tweens.add({
+            targets: this.Lfairy, 
+            y: "-= 50",
+            repeat: -1,
+            yoyo: true
+        });
+        this.tweens.add({
+                targets: this.Rfairy,
+                y: "-= 50",
+                repeat: -1,
+                yoyo: true,
+                delay: 250
+            })
+        this.tweens.add({
+            targets: this.rolypoly,
+            angle: {from: 0, to: 360},
+            repeat: -1
+        })
+        this.tweens.add({
+            targets: this.rolypoly,
+            scale: {from: 0.3, to: 0.5},
+            yoyo: true,
+            repeat: -1
+        })
+
         let menuConfig = {
-            fontFamily: 'Courier',
+            fontFamily: 'Arial',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
+            color: 'white',
+            align: 'center',
             padding: {
             top: 5,
             bottom: 5,
@@ -17,7 +99,8 @@ class Title extends Phaser.Scene {
             fixedWidth: 0
         }
 
-        this.title = this.add.text(game.config.width/2, game.config.height/2, 'THIS IS TEXT', menuConfig).setOrigin(0.5);
+        // title text
+        this.add.text(game.config.width/2, 526, "tap here!", menuConfig).setOrigin(0.5);
         spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE); 
     }
 
@@ -25,5 +108,10 @@ class Title extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(spacebar)){
             this.scene.start('Play');
         }
+
+        // parallax
+        this.bg.tilePositionX += this.SCROLL_SPEED;
+        this.logs.tilePositionX += this.SCROLL_SPEED+1;
+        this.floor.tilePositionX += this.SCROLL_SPEED+1;
     }
 }
